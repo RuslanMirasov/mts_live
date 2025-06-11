9d54e8d1ede52b73614624d76d0ae420352398a1
Ruslan <info@mirasov.dev>
2025-06-11 19:29:32 +0200

    add animation

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -1,1 +1,7 @@
+export const hidePreloader = () => {
+  const body = document.querySelector('.body');
 
+  window.addEventListener('load', () => {
+    body.classList.add('loaded');
+  });
+};
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,7 @@
 import { popup } from './popup.js';
+import { hidePreloader } from './helpers.js';
 
 popup.init();
 window.popup = popup;
+
+hidePreloader();
